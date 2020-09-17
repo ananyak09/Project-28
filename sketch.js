284ce274790a8c62600a7c8daf3a14e8636280c6
Ananya Kedia <69528295+ananyak09@users.noreply.github.com>
2020-09-17 21:41:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var ground,tree,stone,boy;
+var mango1,mango2,mango3,mango4,mango5;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(600,690,1200,20);
+	tree = new Tree(350,90,450,600);
+	stone =new Stone(200,200,40);
+	boy = new Boy(100,515,200,200);
+	mango1 = new Mango(580,300,40);
+  mango2 = new Mango(500,200,40);
+  mango3 = new Mango(650,180,40);
+  mango4 = new Mango(400,300,40);
+  mango5 = new Mango(700,280,40);
+  elastic = new Elastic({x:200,y:200},stone.body);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  detectcollision(stone,mango1);
+  detectcollision(stone,mango2);
+  detectcollision(stone,mango3);
+  detectcollision(stone,mango4);
+  detectcollision(stone,mango5);
+  
+  ground.display();
+  tree.display();
+  stone.display();
+  boy.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  elastic.display();
+  drawSprites();
+ 
+}
+
+
+function mouseDragged(){
+	Matter.Body.setPosition(stone.body, {x: mouseX , y: mouseY});
+ }
+ 
+ 
+ function mouseReleased(){
+	 elastic.fly();
+ }
+ 
+ function detectcollision(lstone,lmango){
+   mangoBodyPosition=lmango.body.position;
+   stoneBodyPosition=lstone.body.position;
+ 
+ var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y);
+ if(distance<=lmango.diameter+lstone.diameter)
+ {
+   Matter.Body.setStatic(lmango.body,false);
+ }
+ }
+ 
+ function keyPressed(){
+   if(keyCode===32){
+	 Matter.Body.setPosition(stone.body,{x:200,y:200});
+	 launcherObject.attach(stone.body);
+	 }
+ }
+
